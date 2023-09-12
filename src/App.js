7ab875552a426e350184909f85a639caa8227d5c
Lucas Gaspari <101724615+gasparinh0@users.noreply.github.com>
2023-09-12 10:49:47 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+/* Imports do React */
+import React, { useState, useEffect } from 'react';
+
+//IMPORTS DAS PÁGINAS
+import HomePage from './componentsWeb/homepage'
+import HomepageMobile from './componentsMobile/HomepageMobile'
+
+function App() {
+
+    //CÓDIGO PARA DETECTAR SE É MOBILE
+    const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 768px)').matches);
+
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(max-width: 768px)');
+      
+      const handleResize = (e) => {
+        setIsMobile(e.matches);
+      };
+
+      mediaQuery.addListener(handleResize);
+
+      return () => {
+        mediaQuery.removeListener(handleResize);
+      };
+    }, []);
+
+    return (
+      <div>
+      {isMobile ? <HomepageMobile /> : <HomePage />}
+      </div>
+    );
+}
+
+export default App;
